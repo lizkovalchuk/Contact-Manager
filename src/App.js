11755ea5a2386b7eeb5e3bb055364a9d48ca4f0b
Liz Kovalchuk <liz.kovalchuk@gmail.com>
2019-04-28 15:19:38 -0400

    header functional component and contact class components created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, { Component } from 'react';
+import Contact from './components/Contact';
+import Header from './components/Header';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>The App Component</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header branding="Contact Manager"/>
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555"/>
+        <Contact name="Karen Smith" email="karen@gmail.com" phone="333-333-3333"/>
+      </div>
+    );
+  }
 }
 
 export default App;
