228c7c13b093ae022b665275dfaadbb515ee6b15
Liz Kovalchuk <liz.kovalchuk@gmail.com>
2019-04-29 17:13:52 -0400

    contact component nested in contacts component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import propTypes from "prop-types";
 
 class Contact extends Component {
+  state = {
+    showContactInfo: true
+  };
+
   render() {
-    const { name, email, phone } = this.props;
+    const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
+
     return (
-      <div>
-        <h4>{name}</h4>
-        <ul>
-           <li>Email: {email}</li>
-           <li>Phone: {phone}</li>
-        </ul>
+      <div className="card card-body mb-3">
+        <h4>
+          {name}<i
+            onClick={() => this.setState({ showContactInfo: !this.state.showContactInfo })}
+            className="fas fa-sort-down"
+          />
+        </h4>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
 }
+
+Contact.propTypes = {
+  contact: propTypes.object.isRequired
+};
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,30 @@
 import React from "react";
+import propTypes from "prop-types";
 
 const Header = props => {
   const { branding } = props;
   return (
-    <div>
-      <h1>{branding}</h1>
-    </div>
+    <nav className="nav-bar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+      <div className="container">
+        <div className="row justify-content-between">
+          <a href="/" className="navbar-brand">{branding}</a>                    
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a href="/" className="nav-link">Home</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
+};
+
+Header.defaultProps = {
+  branding: "My Contacts"
+};
+
+Header.propTypes = {
+  branding: propTypes.string.isRequired
 };
 
 export default Header;
