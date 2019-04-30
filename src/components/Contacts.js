601bc5d42ae57a73d0f69604332e335ff5c3b1b5
Liz Kovalchuk <liz.kovalchuk@gmail.com>
2019-04-29 21:29:22 -0400

    context.js create to utilize context API

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,28 +2,7 @@ import React, { Component } from "react";
 import Contact from "./Contact";
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: "John Doe",
-        email: "jdoe@gmail.com",
-        phone: "555-555-5555"
-      },
-      {
-        id: 2,
-        name: "Karen Williams",
-        email: "karen@gmail.com",
-        phone: "222-222-2222"
-      },
-      {
-        id: 3,
-        name: "Henry Johnson",
-        email: "henry@gmail.com",
-        phone: "111-111-1111"
-      }
-    ]
-  };
+
 
   deleteContact = id => {
     const { contacts } = this.state;
