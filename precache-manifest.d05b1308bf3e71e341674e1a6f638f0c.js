4aa925fff80b6b068bd17041d92800111583325b
Liz Kovalchuk <liz.kovalchuk@gmail.com>
2019-05-17 14:56:19 -0400

    Updates

---
+++ precache-manifest.d05b1308bf3e71e341674e1a6f638f0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a45e11fa719491bc8c751b44973aae",
+    "url": "/Contact-Manager/index.html"
+  },
+  {
+    "revision": "63c931a45b16531ef618",
+    "url": "/Contact-Manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "8cf61982516ce667c51d",
+    "url": "/Contact-Manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "63c931a45b16531ef618",
+    "url": "/Contact-Manager/static/js/2.760bc175.chunk.js"
+  },
+  {
+    "revision": "8cf61982516ce667c51d",
+    "url": "/Contact-Manager/static/js/main.1e4289df.chunk.js"
+  },
+  {
+    "revision": "2d43fcecf4928b0c254b",
+    "url": "/Contact-Manager/static/js/runtime~main.e499c403.js"
+  }
+]);
